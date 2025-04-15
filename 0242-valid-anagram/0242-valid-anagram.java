18625cafee663fbfb60489a6dcab354dc6b5ca60
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2025-04-15 22:45:33 +0300

    Time: 7 ms (35.86%), Space: 45.1 MB (7.46%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        } else {
+            char[] sCharArr = s.toLowerCase().toCharArray();
+            char[] tCharArr = t.toLowerCase().toCharArray();
+            Arrays.sort(sCharArr);
+            Arrays.sort(tCharArr);
+            return Arrays.equals(sCharArr, tCharArr);
+        }
+    }
+}
