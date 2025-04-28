36fa8de1a7d2036adbe8500df17c04d283af7ed1
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2025-04-28 20:45:14 +0300

    Time: 1 ms (65.5%), Space: 41 MB (39.05%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -2,26 +2,20 @@ class Solution {
     public boolean isHappy(int n) {
         Set<Integer> numbers = new HashSet<>();
         while (n != 1) {
-            String numb = String.valueOf(n);
-            int[] arr = new int[numb.length()];
-            for (int i = 0; i < numb.length(); i++) {
-                arr[i] = Integer.parseInt(String.valueOf(numb.charAt(i)));
-            }
-            n = 0;
-            if (arr.length == 1 && (arr[0] == 0 || arr[0] == 2 || arr[0] == 3)) {
-                n = arr[0];
+            int temp = n;
+            int sum = 0;
+            do {
+                int digit = temp % 10;
+                temp = temp / 10;
+                sum += digit * digit;
+            } while (temp > 0);
+            n = sum;
+            if (numbers.contains(n)) {
                 break;
             } else {
-                for(int arrEL : arr) {
-                    n += arrEL * arrEL;
-                }
-                if (numbers.contains(n)) {
-                    break;
-                } else {
-                    numbers.add(n);
-                }
+                numbers.add(n);
             }
         }
-        return n == 1 ? true : false;
+        return n == 1;
     }
 }
