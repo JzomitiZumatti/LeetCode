f88ccae042541e6447374401db4c3f16bfc49530
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2025-05-01 20:02:08 +0300

    Time: 3 ms (41.74%), Space: 41.7 MB (66.31%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,55 +1,24 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        if (s.length() % 2 != 0) {
-            return false;
-        }
-        for (int i = 0; i < s.length(); i++) {
-            char current = s.charAt(i);
-            if (isOpen(current)) {
-                stack.push(current);
-            } else {
-                if (stack.isEmpty()) {
-                    return false;
-                } else if (getIndex(stack.peek()) == getIndex(current)) {
-                    stack.pop();
-                } else {
-                    return false;
-                }
-            }
-        }
-        return stack.isEmpty();
-    }
+        Map<Character, Character> brackets = Map.of(
+        ')', '(', 
+        ']', '[', 
+        '}', '{'
+    );
 
-    public static boolean isOpen(Character c) {
-        char[] openBr = {'(', '[', '{'};
-        for (int i = 0; i < openBr.length; i++) {
-            if (c == openBr[i]) {
-                return true;
-            }
-        }
-        return false;
-    }
+    Stack<Character> stack = new Stack<>();
 
-    public static int getIndex(Character c) {
-        char[] openBr = {'(', '[', '{'};
-        char[] closeBr = {')', ']', '}'};
-        int index = -1;
-        if (isOpen(c)) {
-            for (int i = 0; i < openBr.length; i++) {
-                if (c == openBr[i]) {
-                    index = i;
-                    break;
-                }
+    for (char c : s.toCharArray()) {
+        if (brackets.containsValue(c)) {
+            stack.push(c);
+        } else if (brackets.containsKey(c)) {
+            if (stack.isEmpty() || stack.pop() != brackets.get(c)) {
+                return false;
             }
         } else {
-            for (int i = 0; i < closeBr.length; i++) {
-                if (c == closeBr[i]) {
-                    index = i;
-                    break;
-                }
-            }
+            return false;
         }
-        return index;
+    }
+    return stack.isEmpty();
     }
 }
