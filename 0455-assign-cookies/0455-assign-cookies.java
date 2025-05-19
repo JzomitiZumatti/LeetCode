f689f0c793189cec7c58049b417204edb12f6108
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2025-05-19 20:00:55 +0300

    Time: 9 ms (99.35%), Space: 48.5 MB (73.72%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -1,14 +1,11 @@
 class Solution {
     public static int findContentChildren(int[] g, int[] s) {
-        if (g.length == 0 || s.length == 0) {
-            return 0;
-        }
         int counter = 0;
         Arrays.sort(g);
         Arrays.sort(s);
         int i = 0;
         int j = 0;
-        do {
+        while (i < g.length && j < s.length) {
             if (s[j] >= g[i]) {
                 i++;
                 j++;
@@ -16,7 +13,7 @@ class Solution {
             } else {
                 j++;
             }
-        } while (i < g.length && j < s.length);
+        }
         return counter;
     }
 }
