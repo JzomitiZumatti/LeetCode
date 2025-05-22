55e7a81ce6baa9d786b136f49603f4fd17cf11c2
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2025-05-22 14:29:10 +0300

    Time: 0 ms (100%), Space: 40.7 MB (31.18%) - LeetHub

--- 0476-number-complement/0476-number-complement.java
+++ 0476-number-complement/0476-number-complement.java
@@ -1,16 +1,6 @@
 class Solution {
     public static int findComplement(int num) {
-        List<Integer> numTwo = new ArrayList<>();
-        int number = 0;
-        while (num > 0) {
-            numTwo.add(num % 2);
-            num = num / 2;
-        }
-        numTwo.replaceAll(integer -> integer ^ 1);
-
-        for (int i = 0; i < numTwo.size(); i++) {
-            number += numTwo.get(i) * (int)Math.pow(2, i);
-        }
-        return number;
+        int mask = (1 << Integer.toBinaryString(num).length()) - 1;
+        return num ^ mask;
     }
 }
