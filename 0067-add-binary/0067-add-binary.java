1cd6fed907711e4159bf1bad5bb19339d8b19c92
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2025-05-31 20:28:56 +0300

    Time: 2 ms (42.24%), Space: 42.2 MB (66.45%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -5,39 +5,12 @@ class Solution {
         int sum;
         int aInt;
         int bInt;
-        int length = Math.max(a.length(), b.length());
-        if (a.length() > b.length()) {
-            for (int i = a.length() - 1, j = b.length() - 1; i >= 0 && j >= b.length() - length; i--, j--) {
-                aInt = a.charAt(i) - '0';
-                if (j >= 0) {
-                    bInt = b.charAt(j) - '0';
-                } else {
-                    bInt = 0;
-                }
-                sum = aInt + bInt + carry;
-                carry = sum / 2;
-                total.append(sum % 2);
-            }
-        } else if (a.length() < b.length()) {
-            for (int i = b.length() - 1, j = a.length() - 1; i >= 0 && j >= a.length() - length; i--, j--) {
-                bInt = b.charAt(i) - '0';
-                if (j >= 0) {
-                    aInt = a.charAt(j) - '0';
-                } else {
-                    aInt = 0;
-                }
-                sum = aInt + bInt + carry;
-                carry = sum / 2;
-                total.append(sum % 2);
-            }
-        } else {
-            for (int i = length - 1; i >= 0; i--) {
-                bInt = b.charAt(i) - '0';
-                aInt = a.charAt(i) - '0';
-                sum = aInt + bInt + carry;
-                carry = sum / 2;
-                total.append(sum % 2);
-            }
+        for (int i = a.length() - 1, j = b.length() - 1; i >= 0 || j >= 0; i--, j--) {
+            aInt = i >= 0 ? a.charAt(i) - '0' : 0;
+            bInt = j >= 0 ? b.charAt(j) - '0' : 0;
+            sum = aInt + bInt + carry;
+            carry = sum / 2;
+            total.append(sum % 2);
         }
         if (carry == 1) {
             total.append(carry);
