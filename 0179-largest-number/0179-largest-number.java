d3c89bf18b3329cb846990f45254fd9f7a869a5a
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2025-06-17 16:06:09 +0300

    Time: 6 ms (48.63%), Space: 43.2 MB (72.63%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -1,27 +1,24 @@
 class Solution {
     public String largestNumber(int[] nums) {
-         String[] sNums = new String[nums.length];
-        for(int i = 0; i < nums.length; i++) {
+        String[] sNums = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
             sNums[i] = String.valueOf(nums[i]);
         }
-        StringBuilder stringBuilder = new StringBuilder();
+
         List<String> sorted = new ArrayList<>();
         Collections.addAll(sorted, sNums);
 
-        sorted.sort(new addComparator());
-        if (sorted.get(0).equals("0")) {
-            return "0";
-        }
-        for(String a : sorted) {
-            stringBuilder.append(a);
-        }
-        return stringBuilder.toString();
-    }
+        sorted.sort((p1, p2) -> (p2 + p1).compareTo(p1 + p2));
 
-    static class addComparator implements Comparator<String> {
-        @Override
-        public int compare(String p1, String p2) {
-            return (p2 + p1).compareTo(p1 + p2);
+        if (sorted.get(0).equals("0")) {
+           return "0";
         }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String num : sorted) {
+            stringBuilder.append(num);
+        }
+
+        return stringBuilder.toString();
     }
 }
