38727e7f56cabcda2b51c8c795705362df73ecbb
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2025-06-22 18:24:20 +0300

    Time: 2 ms (93.39%), Space: 55.4 MB (96.44%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.java
+++ 0890-lemonade-change/0890-lemonade-change.java
@@ -1,23 +1,23 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
-        Map<Integer, Integer> availableNotes = new HashMap<>(); //value, quantity
+        int five = 0;
+        int ten = 0;
         for (int b : bills) {
-            if (b == 5) availableNotes.put(b, availableNotes.getOrDefault(b, 0) + 1);
+            if (b == 5) five++;
             if (b == 10) {
-                if (availableNotes.containsKey(5) && availableNotes.get(5) >= 1) {
-                    availableNotes.put(5, availableNotes.getOrDefault(5, 0) - 1);
-                    availableNotes.put(b, availableNotes.getOrDefault(b, 0) + 1);
+                if (five >= 1) {
+                    five--;
+                    ten++;
                 } else {
                     return false;
                 }
             }
             if (b == 20) {
-                if (availableNotes.containsKey(5) && availableNotes.get(5) >= 1
-                        && availableNotes.containsKey(10) && availableNotes.get(10) >= 1) {
-                    availableNotes.put(10, availableNotes.getOrDefault(10, 0) - 1);
-                    availableNotes.put(5, availableNotes.getOrDefault(5, 0) - 1);
-                } else if (availableNotes.containsKey(5) && availableNotes.get(5) >= 3) {
-                    availableNotes.put(5, availableNotes.getOrDefault(5, 0) - 3);
+                if (five >= 1 && ten >= 1) {
+                    five--;
+                    ten--;
+                } else if (five >= 3) {
+                    five -= 3;
                 } else {
                     return false;
                 }
