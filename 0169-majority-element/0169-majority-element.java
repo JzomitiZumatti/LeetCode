f0d5b4478f750f6b43f2b461e0aae6b938d43c90
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2025-07-01 15:06:51 +0300

    Time: 4 ms (49.52%), Space: 53.1 MB (49.33%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,20 +1,6 @@
 class Solution {
     public int majorityElement(int[] nums) {
         Arrays.sort(nums);
-        int[] freq = new int[nums.length];
-        int j = 0;
-        int current = 0;
-        int next = current + 1;
-        int max = 0;
-        while (next < nums.length) {
-            freq[j] += 1;
-            if (nums[current] != nums[next]) {
-                j++;
-            }
-            max = max < freq[j] ? current : max;
-            current++;
-            next++;
-        }
-        return nums[max];
+        return nums[nums.length / 2];
     }
 }
