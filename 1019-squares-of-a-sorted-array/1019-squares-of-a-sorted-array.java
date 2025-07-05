7572eb3f8a86ea3b51c0e4a46903fb13b1666e06
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2025-07-05 23:06:38 +0300

    Time: 7 ms (41.07%), Space: 46.5 MB (87.57%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
@@ -1,23 +1,10 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
         int[] res = new int[nums.length];
-        List<Integer> df = new ArrayList<>();
-        for (int n : nums) {
-            addSorted(df, n * n);
+        for (int i = 0; i < nums.length; i++) {
+            res[i] = nums[i] * nums[i];
         }
-        int i = 0;
-        for (Integer d : df) {
-            res[i] = d;
-            i++;
-        }
+        Arrays.sort(res);
         return res;
-    }
-
-    private static <T extends Comparable<T>> void addSorted(List<T> list, T element) {
-        int index = Collections.binarySearch(list, element);
-        if (index < 0) {
-            index = -(index + 1); // Calculate the insertion point
-        }
-        list.add(index, element);
     }
 }
