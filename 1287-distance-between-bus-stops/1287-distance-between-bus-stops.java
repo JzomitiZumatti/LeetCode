36a93685139beaf0f63d237beb9f66a6760af5cb
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2025-07-12 11:18:19 +0300

    Time: 0 ms (100%), Space: 42.6 MB (46.08%) - LeetHub

--- 1287-distance-between-bus-stops/1287-distance-between-bus-stops.java
+++ 1287-distance-between-bus-stops/1287-distance-between-bus-stops.java
@@ -1,25 +1,19 @@
 class Solution {
     public int distanceBetweenBusStops(int[] distance, int start, int destination) {
-        int max = 0;
         int min = 0;
-        List<Integer> indexes = new ArrayList<>();
         if (start < destination) {
             for (int i = start; i < destination; i++) {
                 min += distance[i];
-                indexes.add(i);
             }
         } else {
             for (int i = destination; i < start; i++) {
                 min += distance[i];
-                indexes.add(i);
             }
         }
-        System.out.println(indexes);
-        for (int i = 0; i < distance.length; i++) {
-            if (!indexes.contains(i)) {
-                max += distance[i];
-            }
+        int sum = 0;
+        for (int j : distance) {
+            sum += j;
         }
-        return Math.min(max, min);
+        return Math.min(min, sum - min);
     }
 }
