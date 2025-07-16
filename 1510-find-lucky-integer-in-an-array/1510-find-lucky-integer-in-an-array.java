181d91bd5e82c15133ba8e017761bec53a07cf53
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2025-07-16 20:04:08 +0300

    Time: 3 ms (66.09%), Space: 43.7 MB (25.03%) - LeetHub

--- 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.java
+++ 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.java
@@ -4,22 +4,24 @@ class Solution {
         int max = 0;
         int counter = 1;
         int maxFreq = 0;
-        for (int i = 1; i < arr.length; i++) {
+        for (int i = arr.length - 1; i >= 1; i--) {
             if (arr[i] == arr[i - 1]) {
                 counter++;
             } else {
-                if (counter == arr[i - 1]) {
+                if (counter == arr[i]) {
                     if (counter > maxFreq) {
                         maxFreq = counter;
-                        max = arr[i - 1];
+                        max = arr[i];
+                    } else {
+                        break;
                     }
                 }
                 counter = 1;
             }
         }
-        if (counter > 0 && counter == arr[arr.length - 1]) {
+        if (counter > 0 && counter == arr[0]) {
             if (counter > maxFreq) {
-                max = arr[arr.length - 1];
+                max = arr[0];
             }
         }
         return max != 0 ? max : -1;
