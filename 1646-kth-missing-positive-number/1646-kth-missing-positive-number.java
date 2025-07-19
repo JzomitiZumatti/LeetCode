5947ed680e633905ae9981684c192f35b0ad7945
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2025-07-19 19:06:09 +0300

    Time: 77 ms (5.04%), Space: 45 MB (5.34%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.java
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.java
@@ -5,15 +5,17 @@ class Solution {
         for (int i = 0; i < arr.length + k; i++) {
             int index = i < arr.length ? i : arr.length - 1;
             counter++;
-            if (counter != arr[index] && counter <= arr[arr.length - 1]) {
+            System.out.println(counter + " " + arr[index]);
+            if (counter != arr[index] && counter <= arr[arr.length - 1] && missing.size() <= k) {
                 while (counter < arr[index]) {
                     missing.add(counter);
                     counter++;
                 }
-            } else if (counter > arr[arr.length - 1]) {
+            } else if (counter > arr[arr.length - 1] && missing.size() <= k) {
                 missing.add(counter);
             }
         }
+        System.out.println(missing);
         return missing.get(k - 1);
     }
 }
