859316c11094608850a29a0a013cf9de6500c867
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2025-07-27 14:26:50 +0300

    Time: 101 ms (43.14%), Space: 49.5 MB (39.7%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,27 +1,21 @@
-class Solution {
+public class Solution {
     public int countPrimes(int n) {
-        if (n <= 1) {
-            return 0;
-        }
+        if (n < 2) return 0;
+
         boolean[] isPrime = new boolean[n];
         Arrays.fill(isPrime, true);
-        isPrime[0] = false;
-        isPrime[1] = false;
-        int counter = 0;
-        for (int i = 2; i < Math.sqrt(n); i++) {
-                if (isPrime[i]) {
-                    int j = i * i;
-                    while (j < n) {
-                        isPrime[j] = false;
-                        j += i;
+
+        for (int i = 2; i * i < n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
                 }
             }
         }
 
-        for (boolean p : isPrime) {
-            if (p) {
-                counter++;
-            }
+        int counter = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) counter++;
         }
 
         return counter;
