d2a8446bcd48e74d787f1e2a18d65aeec6fb2120
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2025-07-29 15:21:47 +0300

    Time: 5 ms (6.42%), Space: 44.4 MB (5.18%) - LeetHub

--- 2386-min-max-game/2386-min-max-game.java
+++ 2386-min-max-game/2386-min-max-game.java
@@ -1,30 +1,24 @@
 class Solution {
     public int minMaxGame(int[] nums) {
-        int state = 1;
-        List<Integer> ns = new ArrayList<>();
-        List<Integer> temp = new ArrayList<>();
-
+        Queue<Integer> ns = new LinkedList<>();
         for (int n : nums) {
             ns.add(n);
         }
-        if (ns.size() < 2) {
-            return ns.get(0);
+        if (ns.size() == 1) {
+            return ns.poll();
         }
-
+        int flag = 0;
         do {
-            for (int i = 0; i < ns.size() - 1; i = i + 2) {
-                int next = i + 1;
-                if (state % 2 != 0) {
-                    temp.add(Math.min(ns.get(i), ns.get(next)));
-                } else {
-                    temp.add(Math.max(ns.get(i), ns.get(next)));
-                }
-                state++;
+            int first = ns.poll();
+            int second = ns.poll();
+            if (flag == 0) {
+                ns.add(Math.min(first, second));
+                flag = 1;
+            } else {
+                ns.add(Math.max(first, second));
+                flag = 0;
             }
-            ns.clear();
-            ns.addAll(temp);
-            temp.clear();
         } while (ns.size() >= 2);
-        return ns.get(0);
+        return ns.poll();
     }
 }
