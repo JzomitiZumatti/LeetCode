ecfb4d769625b53992557172692d3d61921739fa
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2025-08-01 13:26:00 +0300

    Time: 4 ms (94.04%), Space: 44.8 MB (14.61%) - LeetHub

--- 2585-delete-greatest-value-in-each-row/2585-delete-greatest-value-in-each-row.java
+++ 2585-delete-greatest-value-in-each-row/2585-delete-greatest-value-in-each-row.java
@@ -1,40 +1,20 @@
 class Solution {
     public int deleteGreatestValue(int[][] grid) {
-        boolean[][] gr = new boolean[grid.length][grid[0].length];
-        for (boolean[] row : gr) {
-            Arrays.fill(row, true);
+        for (int[] row : grid) {
+            Arrays.sort(row);
         }
         int sum = 0;
-        while (countTrue(gr) >= 1) {
+        int counter = 0;
+        int i = 1;
+        while (counter < grid[0].length) {
             int globMax = Integer.MIN_VALUE;
-            for (int row = 0; row < gr.length; row++) {
-                int max = Integer.MIN_VALUE;
-                int rows = -1;
-                int cols = -1;
-                for (int col = 0; col < gr[row].length; col++) {
-                    if (gr[row][col] && grid[row][col] > max) {
-                        max = grid[row][col];
-                        rows = row;
-                        cols = col;
-                    }
-                }
-                gr[rows][cols] = false;
-                globMax = Math.max(max, globMax);
+            for (int row = 0; row < grid.length; row++) {
+                globMax = Math.max(globMax, grid[row][grid[row].length - i]);
             }
+            i++;
+            counter++;
             sum += globMax;
         }
         return sum;
-    }
-
-    private static int countTrue(boolean[][] arr) {
-        int counter = 0;
-        for (int row = 0; row < arr.length; row++) {
-            for (int col = 0; col < arr[row].length; col++) {
-                if (arr[row][col]) {
-                    counter++;
-                }
-            }
-        }
-        return counter;
     }
 }
