2b1e8e54ccfb9d55b55f7738915eb69df6e202c6
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2025-08-04 14:41:22 +0300

    Time: 4 ms (13.99%), Space: 45.1 MB (7.61%) - LeetHub

--- 2714-left-and-right-sum-differences/2714-left-and-right-sum-differences.java
+++ 2714-left-and-right-sum-differences/2714-left-and-right-sum-differences.java
@@ -1,20 +1,16 @@
 class Solution {
     public int[] leftRightDifference(int[] nums) {
         int[] arr = new int[nums.length];
+        int[] rightArr = new int[nums.length];
+        int[] leftArr = new int[nums.length];
+        for (int i = 0, j = nums.length - 1; i < nums.length & j >= 0; i++, j--) {
+            int lef = Math.max(i - 1, 0);
+            int rig = j + 1 < nums.length ? j + 1 : 0;
+            leftArr[i] = i == 0 ? lef : leftArr[i - 1] + nums[lef];
+            rightArr[j] = j == nums.length - 1 ? rig : rightArr[j + 1] + nums[rig];
+        }
         for (int i = 0; i < nums.length; i++) {
-            int right = 0;
-            int left = 0;
-            if (i != 0) {
-                for (int j = 0; j < i; j++) {
-                    left += nums[j];
-                }
-            }
-            if (i != nums.length - 1) {
-                for (int j = nums.length - 1; j > i; j--) {
-                    right += nums[j];
-                }
-            }
-            arr[i] = Math.abs(left - right);
+            arr[i] = Math.abs(leftArr[i] - rightArr[i]);
         }
         return arr;
     }
