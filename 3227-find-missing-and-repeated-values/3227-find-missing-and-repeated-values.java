0c1cd4a07698cbfce0623765c479a93d35b8105e
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2025-08-08 22:40:54 +0300

    Time: 14 ms (5.83%), Space: 45.4 MB (80.6%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.java
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.java
@@ -2,24 +2,25 @@ class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
         int[] res = new int[2];
         List<Integer> nums = new ArrayList<>();
+        int curSum = 0;
         for (int[] ints : grid) {
             for (int anInt : ints) {
                 nums.add(anInt);
+                curSum += anInt;
             }
+        }
+        int realSum = 0;
+        for (int i = 1; i <= nums.size(); i++) {
+            realSum += i;
         }
         Collections.sort(nums);
         for (int i = 1; i < nums.size(); i++) {
             if (Objects.equals(nums.get(i), nums.get(i - 1))) {
-                res[0] = nums.get(i);
-            } else if (nums.get(i) - nums.get(i - 1) > 1) {
-                res[1] = nums.get(i - 1) + 1;
+                res[0] = nums.get(i - 1);
+                break;
             }
         }
-        if (nums.getLast() < nums.size()) {
-            res[1] = nums.size();
-        } else if (nums.getFirst() > 1) {
-            res[1] = 1;
-        }
+        res[1] = res[0] + (realSum - curSum);
         return res;
     }
 }
