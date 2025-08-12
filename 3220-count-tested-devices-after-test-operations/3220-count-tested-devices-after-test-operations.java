1188d90852b1e2d62997be65c1c93e8ff8cdb84a
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2025-08-12 16:09:35 +0300

    Time: 0 ms (100%), Space: 43.7 MB (10.56%) - LeetHub

--- 3220-count-tested-devices-after-test-operations/3220-count-tested-devices-after-test-operations.java
+++ 3220-count-tested-devices-after-test-operations/3220-count-tested-devices-after-test-operations.java
@@ -1,12 +1,9 @@
 class Solution {
     public int countTestedDevices(int[] batteryPercentages) {
         int counter = 0;
-        for (int i = 0; i < batteryPercentages.length; i++) {
-            if (batteryPercentages[i] > 0) {
+        for (int batteryPercentage : batteryPercentages) {
+            if (batteryPercentage - counter > 0) {
                 counter++;
-                for (int j = i + 1; j < batteryPercentages.length; j++) {
-                    batteryPercentages[j] = batteryPercentages[j] > 0 ? batteryPercentages[j] - 1 : 0;
-                }
             }
         }
         return counter;
