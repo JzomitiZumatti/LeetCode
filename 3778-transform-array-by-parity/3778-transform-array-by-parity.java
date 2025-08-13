03afbf91eb869413727f78f916f465beeadcd9f1
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2025-08-13 11:09:01 +0300

    Time: 187 ms (2.05%), Space: 45.3 MB (6.81%) - LeetHub

--- 3778-transform-array-by-parity/3778-transform-array-by-parity.java
+++ 3778-transform-array-by-parity/3778-transform-array-by-parity.java
@@ -1,17 +1,19 @@
 class Solution {
     public int[] transformArray(int[] nums) {
-        int[] res = new int[nums.length];
-        int i = 0;
+        LinkedList<Integer> sd = new LinkedList<>();
         for (int n : nums) {
             if (n % 2 == 0) {
-                res[i] = 0;
+                sd.addFirst(0);
             } else {
-                res[i] = 1;
+                sd.addLast(1);
             }
-            i++;
         }
-        Arrays.sort(res);
-
+        int[] res = new int[nums.length];
+        int counter = 0;
+        for (Integer s : sd) {
+            res[counter] = s;
+            counter++;
+        }
         return  res;
     }
 }
