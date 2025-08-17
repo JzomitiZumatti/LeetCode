719e6ac299059107702aa6a9f090410d2d5ac608
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2025-08-17 20:29:11 +0300

    Time: 4 ms (16.9%), Space: 43.8 MB (67.52%) - LeetHub

--- 3471-minimum-average-of-smallest-and-largest-elements/3471-minimum-average-of-smallest-and-largest-elements.java
+++ 3471-minimum-average-of-smallest-and-largest-elements/3471-minimum-average-of-smallest-and-largest-elements.java
@@ -1,19 +1,14 @@
 class Solution {
     public double minimumAverage(int[] nums) {
-        PriorityQueue<Integer> sort = new PriorityQueue<>();
         PriorityQueue<Double> averages = new PriorityQueue<>();
-        Deque<Integer> aor = new ArrayDeque<>();
-        for (int n : nums) sort.add(n);
-        while (!sort.isEmpty()) {
-            int wl = sort.poll();
-            aor.add(wl);
-        }
-
-        while (!aor.isEmpty()) {
-            int min = aor.pollFirst();
-            int max = aor.pollLast();
-            double avg = (double) (min + max) / 2;
+        Arrays.sort(nums);
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            double avg = (double) (nums[left] + nums[right]) / 2;
             averages.add(avg);
+            left++;
+            right--;
         }
 
         return !averages.isEmpty() ? averages.poll() : 0;
