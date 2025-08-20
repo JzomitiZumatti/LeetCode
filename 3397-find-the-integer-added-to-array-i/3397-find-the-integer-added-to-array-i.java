97d18a2a64d698433363f1691e379e07386d1c76
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2025-08-20 12:40:57 +0300

    Time: 0 ms (100%), Space: 43.1 MB (75.88%) - LeetHub

---
+++ 3397-find-the-integer-added-to-array-i/3397-find-the-integer-added-to-array-i.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int addedInteger(int[] nums1, int[] nums2) {
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+
+        for (int n : nums1) {
+            min1 = Math.min(min1, n);
+        }
+
+        for (int n : nums2) {
+            min2 = Math.min(min2, n);
+        }
+
+        return min2 - min1;
+    }
+}
