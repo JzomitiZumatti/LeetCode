7486b1aeb567085959da88d3c9b77589a6562bd7
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2025-08-21 14:58:09 +0300

    Time: 0 ms (100%), Space: 45.3 MB (58.07%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.java
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.java
@@ -1,35 +1,22 @@
 class Solution {
     public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
-        Map<Integer, Integer> numbVal = new HashMap<>();
-        for (int i = 0; i < nums1.length; i++) {
-            for (int j = 0; j < nums1[i].length; j++) {
-                numbVal.put(nums1[i][j], numbVal.getOrDefault(nums1[i][j], 0) + nums1[i][j + 1]);
-                break;
+        List<int[]> res = new ArrayList<>();
+        int i = 0, j = 0;
+
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i][0] == nums2[j][0]) {
+                res.add(new int[]{nums1[i][0], nums1[i][1] + nums2[j][1]});
+                i++;
+                j++;
+            } else if (nums1[i][0] < nums2[j][0]) {
+                res.add(nums1[i++]);
+            } else {
+                res.add(nums2[j++]);
             }
         }
+        while (i < nums1.length) res.add(nums1[i++]);
+        while (j < nums2.length) res.add(nums2[j++]);
 
-        for (int i = 0; i < nums2.length; i++) {
-            for (int j = 0; j < nums2[i].length; j++) {
-                numbVal.put(nums2[i][j], numbVal.getOrDefault(nums2[i][j], 0) + nums2[i][j + 1]);
-                break;
-            }
-        }
-
-        int[][] res = new int[numbVal.size()][2];
-
-        int i = 0;
-        for (Map.Entry<Integer, Integer> entry : numbVal.entrySet()) {
-            res[i][0] = entry.getKey();
-            res[i][1] = entry.getValue();
-            i++;
-        }
-        int columnIndexToSort = 0;
-        Arrays.sort(res, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] row1, int[] row2) {
-                return Integer.compare(row1[columnIndexToSort], row2[columnIndexToSort]);
-            }
-        });
-        return res;
+        return res.toArray(new int[res.size()][]);
     }
 }
