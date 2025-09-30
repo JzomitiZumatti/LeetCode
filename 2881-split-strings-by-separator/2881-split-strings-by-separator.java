4881db4832b4a6e31cb61f653e472893c82ea8fb
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2025-09-30 16:50:59 +0300

    Time: 25 ms (15.91%), Space: 46.4 MB (10.24%) - LeetHub

--- 2881-split-strings-by-separator/2881-split-strings-by-separator.java
+++ 2881-split-strings-by-separator/2881-split-strings-by-separator.java
@@ -1,27 +1,15 @@
+import java.util.regex.Pattern;
 class Solution {
     public List<String> splitWordsBySeparator(List<String> words, char separator) {
         List<String> list = new ArrayList<>();
-        StringBuilder stringBuilder = new StringBuilder();
         for (String word : words) {
-            int i = 0;
-            flush(stringBuilder, list);
-            while (i < word.length()) {
-                if (word.charAt(i) != separator) {
-                    stringBuilder.append(word.charAt(i));
-                } else {
-                    flush(stringBuilder, list);
+            String[] parts = word.split(Pattern.quote(String.valueOf(separator)));
+            for (String p : parts) {
+                if (!p.isEmpty()) {
+                    list.add(p);
                 }
-                i++;
             }
         }
-        flush(stringBuilder, list);
         return list;
-    }
-
-    private static void flush(StringBuilder sb, List<String> list) {
-        if (!sb.isEmpty()) {
-            list.add(sb.toString());
-            sb.setLength(0);
-        }
     }
 }
