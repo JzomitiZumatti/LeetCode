cc53de772988f63f1b2e74f4d989a158d33ab0d8
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2025-10-02 16:32:12 +0300

    Time: 20 ms (8.93%), Space: 45.7 MB (26.22%) - LeetHub

--- 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.java
+++ 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.java
@@ -1,21 +1,17 @@
 class Solution {
     public int[] findThePrefixCommonArray(int[] A, int[] B) {
-        int[] res = new int[A.length];
-        Map<Integer, Integer> numbFreq = new HashMap<>();
-        for (int i = 0; i < A.length; i++) {
-            numbFreq.put(A[i], numbFreq.getOrDefault(A[i], 0) + 1);
-            numbFreq.put(B[i], numbFreq.getOrDefault(B[i], 0) + 1);
-            res[i] = getFreq(numbFreq);
-
+        int[] prefixCommon = new int[A.length];
+        int i = 0;
+        while (i < A.length) {
+            int counter = 0;
+            for (int j = 0; j <= i; j++) {
+                for (int k = 0; k <= i; k++) {
+                    if (A[j] == B[k]) counter++;
+                }
+            }
+            prefixCommon[i] = counter;
+            i++;
         }
-        return res;
-    }
-
-    private static int getFreq(Map<Integer, Integer> map) {
-        int counter = 0;
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            counter += entry.getValue() == 2 ? 1 : 0;
-        }
-        return counter;
+        return prefixCommon;
     }
 }
