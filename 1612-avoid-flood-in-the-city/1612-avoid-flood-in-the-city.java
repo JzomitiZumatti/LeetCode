16a9b5ac2bb910d98f6112436d3167260f3d4c3b
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2025-10-07 20:50:11 +0300

    Time: 66 ms (62.56%), Space: 62.1 MB (54.8%) - LeetHub

--- 1612-avoid-flood-in-the-city/1612-avoid-flood-in-the-city.java
+++ 1612-avoid-flood-in-the-city/1612-avoid-flood-in-the-city.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] avoidFlood(int[] rains) {
         int[] res = new int[rains.length];
-        Set<Integer> dryDays = new HashSet<>();
+        TreeSet<Integer> dryDays = new TreeSet<>();
         Map<Integer, Integer> lakeDay = new HashMap<>();
         for (int i = 0; i < rains.length; i++) {
             if (rains[i] == 0) {
@@ -11,14 +11,11 @@ class Solution {
                     res[i] = -1;
                     lakeDay.put(rains[i], i);
                 } else {
-                    int min = Integer.MAX_VALUE;
-                    for (Integer day : dryDays) {
-                        if (day > lakeDay.get(rains[i])) min = Math.min(min, day);
-                    }
-                    if (min != Integer.MAX_VALUE) {
-                        res[min] = rains[i];
+                    Integer dryDay = dryDays.higher(lakeDay.get(rains[i]));
+                    if (dryDay != null) {
+                        res[dryDay] = rains[i];
                         res[i] = -1;
-                        dryDays.remove(min);
+                        dryDays.remove(dryDay);
                         lakeDay.put(rains[i], i);
                     } else {
                         return new int[]{};
@@ -27,10 +24,10 @@ class Solution {
             }
         }
         if (!dryDays.isEmpty()) {
-                for (Integer dryDay : dryDays) {
-                    res[dryDay] = 1;
-                }
+            for (Integer dryDay : dryDays) {
+                res[dryDay] = 1;
             }
+        }
         return res;
     }
 }
