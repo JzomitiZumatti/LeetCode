2973b8f60f35839954da122aa125ed5440388ca7
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2025-10-14 14:09:15 +0300

    Time: 3 ms (20.59%), Space: 44 MB (97.06%) - LeetHub

--- 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.java
+++ 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.java
@@ -2,34 +2,28 @@ class Solution {
     public boolean hasIncreasingSubarrays(List<Integer> nums, int k) {
         if (k == 1 && nums.size() >= 2) return true;
 
-        Map<Integer, List<Integer>> map = new HashMap<>();
+        List<Integer> startPoints = new ArrayList<>();
 
         for (int start = 0; start <= nums.size() - k; start++) {
             boolean isValid = true;
-            List<Integer> l = new ArrayList<>();
 
             for (int i = start; i < start + k - 1; i++) {
                 if (nums.get(i) >= nums.get(i + 1)) {
                     isValid = false;
                     break;
                 }
-                l.add(nums.get(i));
             }
 
             if (isValid) {
-                l.add(nums.get(start + k - 1));
-                map.put(start, new ArrayList<>(l));
+                startPoints.add(start);
             }
         }
 
-        if (map.isEmpty()) return false;
-
-        Integer[] starts = map.keySet().toArray(new Integer[0]);
-        Arrays.sort(starts);
+        Collections.sort(startPoints);
 
         int i = 0;
-        while (i < starts.length) {
-            if (map.containsKey(starts[i] + k)) return true;
+        while (i < startPoints.size()) {
+            if (startPoints.contains(startPoints.get(i) + k) )return true;
             i++;
         }
         return false;
