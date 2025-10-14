07d3375ce1011f53ffcdd5e87fdbc9b37a722d87
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2025-10-14 14:35:56 +0300

    Time: 2 ms (47.79%), Space: 44.6 MB (50%) - LeetHub

--- 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.java
+++ 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.java
@@ -2,23 +2,18 @@ class Solution {
     public boolean hasIncreasingSubarrays(List<Integer> nums, int k) {
         if (k == 1 && nums.size() >= 2) return true;
 
-        Set<Integer> startPoints = new HashSet<>();
-
-        for (int start = 0; start <= nums.size() - k; start++) {
-            boolean isValid = true;
-
-            for (int i = start; i < start + k - 1; i++) {
-                if (nums.get(i) >= nums.get(i + 1)) {
-                    isValid = false;
-                    break;
-                }
+        int start = 0;
+        while (start < nums.size() - k) {
+            int len = 0;
+            for (int i = start; i < nums.size() - (k + 1); i++) {
+                int next = i + 1;
+                if (nums.get(i) < nums.get(next) && nums.get(i + k) < nums.get(next + k)) len++;
+                else break;
+                if (len == k - 1) return true;
             }
-
-            if (isValid) {
-                startPoints.add(start);
-                if (startPoints.contains(start - k)) return true;
-            }
+            start++;
         }
+
         return false;
     }
 }
