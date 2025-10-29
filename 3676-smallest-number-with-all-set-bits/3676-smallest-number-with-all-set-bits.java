05f40718f9771f1b0cc3afe39c765419023aba71
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2025-10-29 18:51:07 +0200

    Time: 0 ms (100%), Space: 40.6 MB (72.64%) - LeetHub

--- 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.java
+++ 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.java
@@ -1,12 +1,8 @@
 class Solution {
     public int smallestNumber(int n) {
-        for (int i = n; i <= Integer.MAX_VALUE; i++) {
-            String s = Integer.toBinaryString(i);
-            Set<Integer> unique = new HashSet<>();
-            for (int j = 0; j < s.length(); j++) {
-                unique.add(Integer.parseInt(String.valueOf(s.charAt(j))));
-            }
-            if (unique.contains(1) && unique.size() == 1) return i;
+        for (int i = 1; i <= n; i++) {
+            int num = (int) Math.pow(2, i) - 1;
+            if (num >= n) return num;
         }
         return 0;
     }
