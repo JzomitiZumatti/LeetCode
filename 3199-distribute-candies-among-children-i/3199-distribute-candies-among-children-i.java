d41fdf409685e7d3989f6573f16af5bd31a2b1ef
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2025-10-30 22:59:01 +0200

    Time: 1 ms (91.89%), Space: 41.1 MB (25.83%) - LeetHub

--- 3199-distribute-candies-among-children-i/3199-distribute-candies-among-children-i.java
+++ 3199-distribute-candies-among-children-i/3199-distribute-candies-among-children-i.java
@@ -1,28 +1,20 @@
-import java.math.BigInteger;
 class Solution {
     public int distributeCandies(int n, int limit) {
-        int k = 3;
-        int m = limit;
-        int upperIndex = n + k - 1;
-        int bottomIndex = k - 1;
-        BigInteger sum = factorial(upperIndex)
-                .divide(factorial(bottomIndex).multiply(factorial(upperIndex - bottomIndex)));
-        for (int i = 1; i <= k; i++) {
-            upperIndex = n - i * (m + 1) + k - 1;
-            int index = (int) Math.pow(-1, i + 1);
-            BigInteger firstC = factorial(k).divide(factorial(i).multiply(factorial(k - i)));
-            BigInteger secondC = upperIndex < 0 ? BigInteger.ZERO :
-                    factorial(upperIndex).divide(factorial(bottomIndex).multiply(factorial(upperIndex - bottomIndex)));
-            sum = sum.subtract(BigInteger.valueOf(index).multiply(firstC).multiply(secondC));
+        int counter = 0;
+        for (int i = 0; i <= Math.min(n, limit); i++) {
+            for (int j = 0; j <= Math.min(n - i, limit); j++) {
+                int x3 = n - i - j;
+                if (x3 >= 0 && x3 <= limit) counter++;
+            }
         }
-        return sum.intValue();
+        return counter;
     }
 
-    private static BigInteger factorial(int n) {
-        if (n == 0) return BigInteger.ONE;
-        BigInteger f = BigInteger.ONE;
+    private static long factorial(int n) {
+        if (n == 0) return 1;
+        long f = 1;
         for (int i = 1; i <= n; i++) {
-            f = f.multiply(BigInteger.valueOf(i));
+            f *= i;
         }
         return f;
     }
