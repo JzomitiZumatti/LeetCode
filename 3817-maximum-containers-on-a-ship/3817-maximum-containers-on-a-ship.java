9eea56fa3b957a8627fc21a200fb69e6a524e499
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2025-11-04 09:55:39 +0200

    Time: 1 ms (21.99%), Space: 42.5 MB (7.26%) - LeetHub

--- 3817-maximum-containers-on-a-ship/3817-maximum-containers-on-a-ship.java
+++ 3817-maximum-containers-on-a-ship/3817-maximum-containers-on-a-ship.java
@@ -1,12 +1,5 @@
 class Solution {
     public int maxContainers(int n, int w, int maxWeight) {
-        int cells = n * n;
-        int counter = 0;
-        for (int i = 1; i <= cells; i++) {
-            if (w * i <= maxWeight) {
-                counter = i;
-            } else break;
-        }
-        return counter;
+        return Math.min(n * n, maxWeight / w);
     }
 }
