4f332ed7c386a8f5622c2e91f58a5b04daec6449
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2025-11-17 11:18:15 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1548-check-if-all-1s-are-at-least-length-k-places-away/1548-check-if-all-1s-are-at-least-length-k-places-away.java
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/1548-check-if-all-1s-are-at-least-length-k-places-away.java
@@ -1,16 +1,30 @@
 class Solution {
     public boolean kLengthApart(int[] nums, int k) {
-        int zeroCounter = 0;
-        int oneCounter = 0;
-        for (int num : nums) {
-            if (num == 1) {
-                oneCounter++;
-                if (oneCounter > 1) {
-                    if (zeroCounter < k) return false;
-                    else zeroCounter = 0;
-                }
-            } else if (oneCounter > 0) zeroCounter++;
+        StringBuilder stringBuilder = new StringBuilder();
+        int first = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                first = i;
+                break;
+            }
+        }
 
+        int last = -1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] == 1) {
+                last = i;
+                break;
+            }
+        }
+
+        for (int i = first; i <= last; i++) {
+            stringBuilder.append(nums[i]);
+        }
+        String[] arrZeros = stringBuilder.toString().split("1");
+
+        if (arrZeros.length == 0 && k > 0) return false;
+        for (String zeros : arrZeros) {
+            if (!zeros.isEmpty() && zeros.length() < k) return false;
         }
         return true;
     }
