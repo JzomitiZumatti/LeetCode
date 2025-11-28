a9feecf955f5af810d501eda3345b5f78288f852
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2025-11-28 10:09:38 +0200

    Time: 10 ms (6.18%), Space: 42.9 MB (5.03%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,16 +1,15 @@
 class Solution {
     public int reverseBits(int n) {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(to32Binary(n));
-        return Integer.parseInt(stringBuilder.reverse().toString(), 2);
+        return Integer.parseInt(to32Binary(n).reverse().toString(), 2);
     }
 
-    private static String to32Binary(int n) {
-        String binaryString = Integer.toBinaryString(n);
-        if (binaryString.length() < 32) {
-            return String.format("%32s", binaryString).replace(' ', '0');
+    private static StringBuilder to32Binary(int n) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(Integer.toBinaryString(n));
+        if (stringBuilder.length() < 32) {
+            return new StringBuilder(String.format("%32s", stringBuilder).replace(' ', '0'));
         } else {
-            return binaryString;
+            return stringBuilder;
         }
     }
 }
