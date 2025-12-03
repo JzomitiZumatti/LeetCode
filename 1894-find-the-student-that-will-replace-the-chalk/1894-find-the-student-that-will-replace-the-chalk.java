76e787027e1ff86515dbb49d444df83fc136804e
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2025-12-03 21:55:58 +0200

    Time: 2033 ms (5.09%), Space: 81.6 MB (46.19%) - LeetHub

--- 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.java
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.java
@@ -3,7 +3,7 @@ class Solution {
         int pos = -1;
         while (k >= 0) {
             for (int i = 0; i < chalk.length; i++) {
-                if (k - chalk[i] < 0) return i;
+                if (k < chalk[i]) return i;
                 else k -= chalk[i];
             }
         }
