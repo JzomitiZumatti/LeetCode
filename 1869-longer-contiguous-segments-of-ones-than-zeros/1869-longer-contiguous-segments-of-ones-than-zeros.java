10e6728128d8e3a93bedfdac7e338b34691b7ba7
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2025-12-13 12:19:18 +0200

    Time: 1 ms (74.47%), Space: 42.3 MB (66.97%) - LeetHub

--- 1869-longer-contiguous-segments-of-ones-than-zeros/1869-longer-contiguous-segments-of-ones-than-zeros.java
+++ 1869-longer-contiguous-segments-of-ones-than-zeros/1869-longer-contiguous-segments-of-ones-than-zeros.java
@@ -1,41 +1,20 @@
 class Solution {
     public boolean checkZeroOnes(String s) {
-        if (s.length() == 1 && s.charAt(0) == '1') return true;
-        int zeroMax = 0;
-        int onesMax = 0;
-        int zeroCounter = 0;
-        int onesCounter = 0;
+        char previous = s.charAt(0);
+        int maxZero = 0;
+        int maxOnes = 0;
+        int currentStreak = 1;
         for (int i = 1; i < s.length(); i++) {
-            boolean b = s.charAt(i - 1) == s.charAt(i);
-            if (b && s.charAt(i - 1) == '0') {
-                if (onesCounter > 0) {
-                    onesMax = Math.max(onesMax, onesCounter + 1);
-                    onesCounter = 0;
-                }
-                zeroCounter++;
-            } else if (b && s.charAt(i - 1) == '1') {
-                if (zeroCounter > 0) {
-                    zeroMax = Math.max(zeroMax, zeroCounter + 1);
-                    zeroCounter = 0;
-                }
-                onesCounter++;
-            } else {
-                if (zeroCounter > 0) {
-                    zeroMax = Math.max(zeroMax, zeroCounter + 1);
-                    zeroCounter = 0;
-                }
-                if (onesCounter > 0) {
-                    onesMax = Math.max(onesMax, onesCounter + 1);
-                    onesCounter = 0;
-                }
+            if (previous == s.charAt(i)) currentStreak++;
+            else {
+                if (previous == '0') maxZero = Math.max(maxZero, currentStreak);
+                else maxOnes = Math.max(maxOnes, currentStreak);
+                previous = s.charAt(i);
+                currentStreak = 1;
             }
         }
-        if (zeroCounter > 0) {
-            zeroMax = Math.max(zeroMax, zeroCounter + 1);
-        }
-        if (onesCounter > 0) {
-            onesMax = Math.max(onesMax, onesCounter + 1);
-        }
-        return onesMax > zeroMax;
+        if (previous == '0') maxZero = Math.max(maxZero, currentStreak);
+        else maxOnes = Math.max(maxOnes, currentStreak);
+        return maxOnes > maxZero;
     }
 }
