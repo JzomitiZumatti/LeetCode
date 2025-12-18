6b024fd54fd6a6b8750e69ef572b018a2bf3d1f8
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2025-12-18 15:33:56 +0200

    Time: 4 ms (27.13%), Space: 46.7 MB (10.37%) - LeetHub

--- 2190-most-frequent-number-following-key-in-an-array/2190-most-frequent-number-following-key-in-an-array.java
+++ 2190-most-frequent-number-following-key-in-an-array/2190-most-frequent-number-following-key-in-an-array.java
@@ -1,19 +1,9 @@
 class Solution {
     public int mostFrequent(int[] nums, int key) {
         Map<Integer, Integer> numFreq = new HashMap<>();
-        int previous = nums[0];
-        boolean isKey = false;
         for (int i = 1; i < nums.length; i++) {
             if (nums[i - 1] == key) {
-                previous = nums[i];
                 numFreq.put(nums[i], numFreq.getOrDefault(nums[i], 0) + 1);
-                isKey = true;
-            }
-            if (isKey && nums[i] == previous) {
-                numFreq.put(nums[i], numFreq.getOrDefault(nums[i], 0) + 1);
-            } else {
-                isKey = false;
-                previous = nums[i];
             }
         }
         int max = -1;
