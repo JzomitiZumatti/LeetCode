dea91f930187db92659bd5c0b5f688c951f542bf
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2025-12-30 14:56:18 +0200

    Time: 10 ms (5.63%), Space: 45.9 MB (5.05%) - LeetHub

--- 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.java
+++ 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.java
@@ -1,14 +1,11 @@
 class Solution {
     public int secondHighest(String s) {
-        PriorityQueue<Integer> re = new PriorityQueue<>();
-        for (int i = 0; i < s.length(); i++) {
-            if (Character.isDigit(s.charAt(i))) re.add(Integer.parseInt(String.valueOf(s.charAt(i))));
-        }
-        if (re.isEmpty()) return -1;
         List<Integer> nums = new ArrayList<>();
-        while (!re.isEmpty()) {
-            nums.add(re.poll());
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isDigit(s.charAt(i))) nums.add(Integer.parseInt(String.valueOf(s.charAt(i))));
         }
+        if (nums.isEmpty()) return -1;
+        nums.sort(Integer::compareTo);
         for (int i = nums.size() - 1; i >= 1; i--) {
             if (nums.get(i - 1) < nums.get(i)) return nums.get(i - 1);
         }
