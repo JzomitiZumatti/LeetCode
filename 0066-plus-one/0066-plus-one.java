36bb3f3f53726e6d4eba2a770e9b4069eb353478
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2026-01-01 20:31:40 +0200

    Time: 0 ms (100%), Space: 43.9 MB (7.44%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -2,31 +2,20 @@ import java.math.BigInteger;
 
 class Solution {
     public int[] plusOne(int[] digits) {
-        List<Integer> numbers = new ArrayList<>(Arrays.stream(digits).boxed().toList());
-        Integer lastEl = numbers.getLast();
-        StringBuilder str = new StringBuilder();
-        if (lastEl != 9 ) {
-            numbers.set(numbers.size() - 1, numbers.getLast() + 1);
-        } else {
-            List<Integer> numbersCopy = List.copyOf(numbers);
-            System.out.println(String.valueOf(numbersCopy));
-            for (int i = numbersCopy.size() - 1; i >=  0; i--) {
-                if (numbersCopy.get(i) == 9) {
-                    str.append(numbersCopy.get(i));
-                    numbers.removeLast();
-                } else {
-                    str.append(numbersCopy.get(i));
-                    numbers.removeLast();
-                    break;
+        int[] result = new int[digits.length + 1];
+        boolean flag = false;
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i] += 1;
+                break;
+            } else if (digits[i] == 9) {
+                digits[i] = 0;
+                if (i == 0) {
+                    result[0] = 1;
+                    flag = true;
                 }
             }
-            str.reverse();
-            BigInteger bigInt = new BigInteger(String.valueOf(str)).add(new BigInteger("1"));
-            String ho = String.valueOf(bigInt);
-            for (int i = 0; i < ho.length(); i++) {
-                numbers.addLast(Integer.parseInt(String.valueOf(ho.charAt(i))));
-            }
         }
-        return numbers.stream().mapToInt(i -> i).toArray();
+        return flag ? result : digits;
     }
 }
