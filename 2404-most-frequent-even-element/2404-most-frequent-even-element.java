9b41bdc0424515ad574853fe962d5b374d4ffa38
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2026-01-27 19:37:50 +0200

    Time: 67 ms (7.35%), Space: 48.2 MB (7.28%) - LeetHub

---
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int mostFrequentEven(int[] nums) {
+        Map<Integer, Integer> evenFreq = new HashMap<>();
+        for (int num : nums) {
+            if (num % 2 == 0) evenFreq.put(num, evenFreq.getOrDefault(num, 0) + 1);
+        }
+        if (evenFreq.isEmpty()) return -1;
+        Map<Integer, Integer> sortedMapDescending = evenFreq.entrySet()
+                .stream()
+                .sorted(Map.Entry.<Integer, Integer>comparingByValue().reversed())
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (e1, e2) -> e1,
+                        LinkedHashMap::new
+                ));
+        System.out.println(sortedMapDescending);
+        int max = -1;
+        int min = Integer.MAX_VALUE;
+        for (Map.Entry<Integer, Integer> entry : sortedMapDescending.entrySet()) {
+            if (entry.getValue() == max) {
+                min = Math.min(min, entry.getKey());
+            } else if (entry.getValue() > max) {
+                max = entry.getValue();
+                min = entry.getKey();
+            }
+        }
+        return min == Integer.MAX_VALUE ? -1 : min;
+    }
+}
