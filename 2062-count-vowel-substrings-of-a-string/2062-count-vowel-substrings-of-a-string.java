4d53377bde15efddcf70c7702515e17ff59bfaf9
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2026-02-11 21:54:42 +0200

    Time: 16 ms (18.12%), Space: 43.7 MB (44.67%) - LeetHub

--- 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.java
+++ 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.java
@@ -1,32 +1,27 @@
 class Solution {
     public int countVowelSubstrings(String word) {
         int counter = 0;
-        for (int i = 0; i < word.length(); i++) {
-            Set<Character> vovels = new HashSet<>();
-            char c = word.charAt(i);
-            if (isVovel(c)) {
-                vovels.add(c);
-                for (int j = i + 1; j < word.length(); j++) {
-                    char cc = word.charAt(j);
-                    if (isVovel(cc)) {
-                        vovels.add(cc);
-                        if (vovels.size() >= 5) counter++;
-                    }
-                    else break;
-                }
-            }
-            vovels.clear();
-        }
-        return counter;
-    }
-
-    private static boolean isVovel(char c) {
         Set<Character> vovels = new HashSet<>();
         vovels.add('a');
         vovels.add('e');
         vovels.add('i');
         vovels.add('o');
         vovels.add('u');
-        return vovels.contains(c);
+        for (int i = 0; i < word.length(); i++) {
+            Set<Character> sub = new HashSet<>();
+            char c = word.charAt(i);
+            if (vovels.contains(c)) {
+                sub.add(c);
+                for (int j = i + 1; j < word.length(); j++) {
+                    char cc = word.charAt(j);
+                    if (vovels.contains(cc)) {
+                        sub.add(cc);
+                        if (sub.size() >= 5) counter++;
+                    }
+                    else break;
+                }
+            }
+        }
+        return counter;
     }
 }
