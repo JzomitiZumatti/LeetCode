5cfe97ce02d3c146486a4f9ae689e65b417655fe
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2026-02-14 21:38:14 +0200

    Time: 36 ms (67.78%), Space: 47.1 MB (19.76%) - LeetHub

--- 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.java
+++ 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.java
@@ -1,29 +1,23 @@
 class Solution {
     public int longestBalanced(String s) {
         int size = 1;
-        Map<Character, Integer> letterPos = new HashMap<>();
-        int p = 0;
-        for (char c = 'a'; c <= 'z'; c++) {
-            letterPos.put(c, p);
-            p++;
-        }
         for (int i = 0; i < s.length() - 1; i++) {
             int[] freqs = new int[26];
             int maxFreq = 0;
             int unique = 0;
-            char c = s.charAt(i);
-            if (freqs[letterPos.get(c)] == 0) {
+            int posI = s.charAt(i) - 'a';
+            if (freqs[posI] == 0) {
                 unique++;
             }
-            freqs[letterPos.get(c)]++;
-            maxFreq = Math.max(maxFreq, freqs[letterPos.get(c)]);
+            freqs[posI]++;
+            maxFreq = Math.max(maxFreq, freqs[posI]);
             for (int j = i + 1; j < s.length(); j++) {
-                char cc = s.charAt(j);
-                if (freqs[letterPos.get(cc)] == 0) {
+                int posJ = s.charAt(j) - 'a';
+                if (freqs[posJ] == 0) {
                     unique++;
                 }
-                freqs[letterPos.get(cc)]++;
-                maxFreq = Math.max(maxFreq, freqs[letterPos.get(cc)]);
+                freqs[posJ]++;
+                maxFreq = Math.max(maxFreq, freqs[posJ]);
                 if (maxFreq * unique == j - i + 1) size = Math.max(size, j - i + 1);
             }
         }
