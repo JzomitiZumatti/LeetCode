d9cb73b82dd00c6cda5aba621f8f85641ae8c45b
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2026-02-17 09:33:11 +0200

    Time: 5 ms (46.73%), Space: 46.8 MB (25.63%) - LeetHub

--- 1417-reformat-the-string/1417-reformat-the-string.java
+++ 1417-reformat-the-string/1417-reformat-the-string.java
@@ -1,5 +1,28 @@
 class Solution {
     public String reformat(String s) {
+        if (isAlreadyFormated(s)) return s;
+        List<Character> letters = new ArrayList<>();
+        List<Character> numbers = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) numbers.add(c);
+            else letters.add(c);
+        }
+        if (letters.isEmpty() || numbers.isEmpty()) return "";
+        if (Math.abs(letters.size() - numbers.size()) > 1) return "";
+        StringBuilder stringBuilder = new StringBuilder();
+        List<Character> a = numbers.size() >= letters.size() ? numbers : letters;
+        List<Character> b = numbers.size() >= letters.size() ? letters : numbers;
+        for (int i = 0; i < a.size(); i++) {
+            stringBuilder.append(a.get(i));
+            if (i < b.size()) {
+                stringBuilder.append(b.get(i));
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    private static boolean isAlreadyFormated(String s) {
         char first = s.charAt(0);
         boolean isGood = true;
         for (int i = 1; i < s.length(); i++) {
@@ -13,37 +36,6 @@ class Solution {
             }
             first = s.charAt(i);
         }
-        if (isGood) return s;
-        List<Character> letters = new ArrayList<>();
-        List<Integer> numbers = new ArrayList<>();
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (Character.isDigit(c)) numbers.add(Integer.parseInt(String.valueOf(c)));
-            else letters.add(c);
-        }
-        if (letters.isEmpty() || numbers.isEmpty()) return "";
-        if (Math.abs(letters.size() - numbers.size()) > 1) return "";
-        StringBuilder stringBuilder = new StringBuilder();
-        boolean numberIsLonger = numbers.size() >= letters.size();
-        int i = 0;
-        int j = 0;
-        while (i < numbers.size() || j < letters.size()) {
-            if (numberIsLonger) {
-                if (i < numbers.size()) {
-                    stringBuilder.append(numbers.get(i++));
-                }
-                if (j < letters.size()) {
-                    stringBuilder.append(letters.get(j++));
-                }
-            } else {
-                if (j < letters.size()) {
-                    stringBuilder.append(letters.get(j++));
-                }
-                if (i < numbers.size()) {
-                    stringBuilder.append(numbers.get(i++));
-                }
-            }
-        }
-        return stringBuilder.toString();
+        return isGood;
     }
 }
