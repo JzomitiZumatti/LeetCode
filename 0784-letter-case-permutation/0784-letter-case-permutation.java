26e9ba009a49ab2e25bdea6be07b43dbc2af48ea
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2026-02-18 10:45:51 +0200

    Time: 3 ms (43.37%), Space: 47 MB (97.85%) - LeetHub

---
+++ 0784-letter-case-permutation/0784-letter-case-permutation.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<String> letterCasePermutation(String s) {
+        List<String> path = new ArrayList<>();
+        dfs(s, 0, new StringBuilder(), path);
+        return path;
+    }
+
+    private static void dfs(String s, int i, StringBuilder stringBuilder, List<String> res) {
+        if (stringBuilder.length() == s.length()) {
+            res.add(stringBuilder.toString());
+            return;
+        }
+        char c = s.charAt(i);
+        if (Character.isDigit(c)) {
+            stringBuilder.append(c);
+        } else {
+            stringBuilder.append(String.valueOf(c).toLowerCase());
+            dfs(s, i + 1, stringBuilder, res);
+            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+            stringBuilder.append(String.valueOf(c).toUpperCase());
+        }
+        dfs(s, i + 1, stringBuilder, res);
+        stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+    }
+}
