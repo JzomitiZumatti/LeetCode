633c0bd0926d6d827277ba3413719b3b59102cf7
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2026-02-20 15:56:59 +0200

    Time: 1 ms (100%), Space: 80 MB (31.32%) - LeetHub

--- 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.java
+++ 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.java
@@ -5,15 +5,6 @@ class Solution {
             x ^= num;
         }
         if (x == k) return 0;
-        int totalBits = Math.max(Integer.toBinaryString(k).length(), Integer.toBinaryString(x).length());
-        String need = String.format("%" + totalBits + "s", Integer.toBinaryString(k)).replace(' ', '0');
-        String have = String.format("%" + totalBits + "s", Integer.toBinaryString(x)).replace(' ', '0');
-        int counter = 0;
-        for (int i = 0; i < need.length(); i++) {
-            char nC = need.charAt(i);
-            char hC = have.charAt(i);
-            if (nC != hC) counter++;
-        }
-        return counter;
+        return Integer.bitCount(x ^ k);
     }
 }
