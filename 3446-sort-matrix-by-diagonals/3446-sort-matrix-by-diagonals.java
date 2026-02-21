88bc2caa48b7bd5dd8e7f6d00d63aae8d7392c7f
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2026-02-21 23:48:36 +0200

    Time: 9 ms (21.26%), Space: 47.5 MB (68.2%) - LeetHub

--- 3446-sort-matrix-by-diagonals/3446-sort-matrix-by-diagonals.java
+++ 3446-sort-matrix-by-diagonals/3446-sort-matrix-by-diagonals.java
@@ -10,15 +10,12 @@ class Solution {
             if (entry.getKey() >= 0) entry.getValue().sort(Comparator.naturalOrder());
             else entry.getValue().sort(Comparator.reverseOrder());
         }
-        Map<Integer, Integer> difSize = new HashMap<>();
-        for (Map.Entry<Integer, List<Integer>> entry : difLsit.entrySet()) {
-            difSize.put(entry.getKey(), entry.getValue().size() - 1);
-        }
+
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
                 int n =  i - j;
-                grid[i][j] = difLsit.get(n).get(difSize.get(n));
-                difSize.put(n, difSize.get(n) - 1);
+                List<Integer> list = difLsit.get(n);
+                grid[i][j] = list.removeLast();
             }
         }
         return grid;
