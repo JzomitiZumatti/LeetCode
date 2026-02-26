dcfd318f477f5fb05c05de40bacfa8e9a3bc79e3
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2026-02-26 12:19:35 +0200

    Time: 12 ms (15.61%), Space: 43 MB (70.73%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String countAndSay(int n) {
+        String[] dp = new String[n];
+        dp[0] = "1";
+        int i = 1;
+        while (i < n) {
+            int counter = 1;
+            StringBuilder stringBuilder = new StringBuilder();
+            int prev = dp[i -1].charAt(0) - '0';
+            for (int j = 1; j < dp[i - 1].length(); j++) {
+                int current = dp[i -1].charAt(j) - '0';
+                if (prev == current) counter++;
+                else {
+                    stringBuilder.append(counter).append(prev);
+                    counter = 1;
+                    prev = current;
+                }
+            }
+            boolean lastMissed = prev == Integer.parseInt(String.valueOf(dp[i - 1].charAt(dp[i - 1].length() - 1)));
+            if (stringBuilder.isEmpty() || lastMissed) stringBuilder.append(counter).append(prev);
+            dp[i] = stringBuilder.toString();
+            i++;
+        }
+        return dp[n -1];
+    }
+}
