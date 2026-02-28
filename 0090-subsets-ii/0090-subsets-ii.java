0aa6ea458724b25daf9a41c609fee5866dac08e7
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2026-03-01 00:56:32 +0200

    Time: 2 ms (99.48%), Space: 45.3 MB (23.86%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -3,31 +3,18 @@ class Solution {
         Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> path = new ArrayList<>();
-        boolean[] isAllowed = new boolean[nums.length];
-        Arrays.fill(isAllowed, true);
-        dfs(nums, res, path, isAllowed);
+        int start = 0;
+        dfs(start, nums, res, path);
         return res;
     }
 
-    private static void dfs(int[] nums, List<List<Integer>> res, List<Integer> path, boolean[] isAllowed) {
+    private static void dfs(int start, int[] nums, List<List<Integer>> res, List<Integer> path) {
         res.add(new ArrayList<>(path));
-        for (int i = 0; i < nums.length; i++) {
-            if (i > 0 && nums[i - 1] == nums[i] && isAllowed[i - 1]) continue;
-            if (!isAllowed[i]) continue;
-            if (path.isEmpty()) {
-                path.add(nums[i]);
-                isAllowed[i] = false;
-                dfs(nums, res, path, isAllowed);
-                path.removeLast();
-                isAllowed[i] = true;
-            }
-            if (!path.isEmpty() && path.getLast() <= nums[i]) {
-                path.add(nums[i]);
-                isAllowed[i] = false;
-                dfs(nums, res, path, isAllowed);
-                path.removeLast();
-                isAllowed[i] = true;
-            }
+        for (int i = start; i < nums.length; i++) {
+            if (i > start && nums[i] == nums[i - 1]) continue;
+            path.add(nums[i]);
+            dfs(i + 1, nums, res, path);
+            path.removeLast();
         }
     }
 }
