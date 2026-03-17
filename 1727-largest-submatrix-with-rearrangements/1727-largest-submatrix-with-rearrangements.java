19be173df7268ce169ee43125296a62914ccd4c6
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2026-03-17 13:32:24 +0200

    Time: 16 ms (33.04%), Space: 115.9 MB (9.57%) - LeetHub

--- 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.java
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.java
@@ -4,17 +4,15 @@ class Solution {
         int[][] heights = new int[matrix.length][matrix[0].length];
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
-                if (i == 0) heights[i][j] += matrix[i][j];
+                if (i == 0) heights[i][j] = matrix[i][j];
                 else if (matrix[i][j] == 1) heights[i][j] = heights[i - 1][j] + 1;
                 else heights[i][j] = 0;
             }
         }
         for (int[] height : heights) {
             Arrays.sort(height);
-            int j = 0;
             for (int i = height.length - 1; i >= 0; i--) {
-                res = Math.max(res, height[i] * (j + 1));
-                j++;
+                res = Math.max(res, height[i] * (height.length - i));
             }
         }
         return res;
