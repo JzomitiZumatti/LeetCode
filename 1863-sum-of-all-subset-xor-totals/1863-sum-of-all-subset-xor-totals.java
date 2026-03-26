8f1ec278be85879a9d2377d00c8c0ae0cdd708f6
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2026-03-26 13:19:07 +0200

    Time: 6 ms (16.8%), Space: 44.1 MB (10.91%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
@@ -1,26 +1,20 @@
 class Solution {
     public int subsetXORSum(int[] nums) {
-        int res = 0;
-        List<List<Integer>> r = new ArrayList<>();
-        List<Integer> path = new ArrayList<>();
+        List<Integer> subsetsSum = new ArrayList<>();
         int start = 0;
-        dfs(start, nums, r, path);
-        for (List<Integer> list : r) {
-            int xor = 0;
-            for (Integer i : list) {
-                xor ^= i;
-            }
-            res += xor;
+        int currentSum = 0;
+        dfs(start, nums, currentSum, subsetsSum);
+        int sum = 0;
+        for (Integer n : subsetsSum) {
+            sum += n;
         }
-        return res;
+        return sum;
     }
 
-    private static void dfs(int start, int[] nums, List<List<Integer>> r, List<Integer> path) {
-        r.add(new ArrayList<>(path));
+    private static void dfs(int start, int[] nums, int currentSum, List<Integer> subsetsSum) {
+        subsetsSum.add(currentSum);
         for (int i = start; i < nums.length; i++) {
-            path.add(nums[i]);
-            dfs(i + 1, nums, r, path);
-            path.remove(path.size() - 1);
+            dfs(i + 1, nums, currentSum ^ nums[i], subsetsSum);
         }
     }
 }
