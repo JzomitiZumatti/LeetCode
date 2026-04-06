30d2bb1bfa38f8a49dd8d983fc44a54742aa260e
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2026-04-06 18:55:20 +0300

    Time: 2 ms (5.1%), Space: 44.6 MB (12.63%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        int start = 0;
+        Arrays.sort(nums);
+        dfs(start, nums, ans, path);
+        return ans;
+    }
+
+    private static void dfs(int start, int[] nums, List<List<Integer>> ans, List<Integer> path) {
+        ans.add(new ArrayList<>(path));
+        for (int i = start; i < nums.length; i++) {
+            path.add(nums[i]);
+            dfs(i + 1, nums, ans, path);
+            path.removeLast();
+        }
+    }
+}
