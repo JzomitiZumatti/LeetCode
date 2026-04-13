2f7f8262fa18de673f29d06c7e66c9bdfdcff816
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2026-04-13 21:08:53 +0300

    Time: 3 ms (96.75%), Space: 47.1 MB (81.51%) - LeetHub

---
+++ 3746-minimum-string-length-after-balanced-removals/3746-minimum-string-length-after-balanced-removals.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minLengthAfterRemovals(String s) {
+        int a = 0;
+        int b = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == 'a') a++;
+            else b++;
+        }
+        return Math.abs(a - b);
+    }
+}
