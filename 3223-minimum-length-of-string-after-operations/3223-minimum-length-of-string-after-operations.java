0cdd41d4934523076d4cf203e5a7a7a4f305c52b
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2026-04-21 14:06:41 +0300

    Time: 9 ms (96.14%), Space: 48.2 MB (94.2%) - LeetHub

--- 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.java
+++ 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.java
@@ -2,17 +2,16 @@ class Solution {
     public int minimumLength(String s) {
         int ans = 0;
         int[] freq = new int[26];
-        Set<Character> unique = new HashSet<>();
         for (int i = 0; i < s.length(); i++) {
             int ascii = s.charAt(i) - 'a';
             freq[ascii]++;
-            unique.add(s.charAt(i));
         }
-        for (Character c : unique) {
-            int f = freq[c - 'a'];
-            if (f < 3) ans += f;
-            else if (f % 2 != 0) ans += 1;
-            else ans += 2;
+        for (int f : freq) {
+            if (f > 0) {
+                if (f < 3) ans += f;
+                else if (f % 2 != 0) ans += 1;
+                else ans += 2;
+            }
         }
         return ans;
     }
