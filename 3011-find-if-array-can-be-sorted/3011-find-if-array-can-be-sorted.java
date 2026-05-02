9f15defe65cc1518a1e15830913602be5ae932f7
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2026-05-02 14:43:31 +0300

    Time: 7 ms (33.78%), Space: 46.4 MB (62.16%) - LeetHub

--- 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.java
+++ 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.java
@@ -1,6 +1,5 @@
 class Solution {
     public boolean canSortArray(int[] nums) {
-        if (isSorted(nums)) return true;
         List<List<Integer>> groups = new ArrayList<>();
         groups.add(new ArrayList<>());
         groups.getFirst().add(nums[0]);
@@ -16,20 +15,10 @@ class Solution {
             }
         }
         Collections.sort(groups.get(cur));
-        System.out.println(groups);
         for (int i = 1; i < groups.size(); i++) {
             int max = groups.get(i - 1).getLast();
             int min = groups.get(i).getFirst();
             if (max > min) return false;
-        }
-        return true;
-    }
-
-    private static boolean isSorted(int[] arr) {
-        for (int i = 0; i < arr.length - 1; i++) {
-            if (arr[i] > arr[i + 1]) {
-                return false;
-            }
         }
         return true;
     }
