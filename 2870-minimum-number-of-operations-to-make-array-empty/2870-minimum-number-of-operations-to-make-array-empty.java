8bd228520dbbff21130e1793e54a0083e23c13f2
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2026-05-03 17:38:44 +0300

    Time: 103 ms (5.28%), Space: 109.1 MB (5.28%) - LeetHub

---
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        int ans = 0;
+        Map<Integer, List<Integer>> numPos = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            numPos.putIfAbsent(nums[i], new ArrayList<>());
+            numPos.get(nums[i]).add(i);
+        }
+        System.out.println(numPos);
+        for (List<Integer> value : numPos.values()) {
+            if (value.size() == 1) return -1;
+            else ans += (value.size() + 2) / 3;
+        }
+        return ans;
+    }
+}
