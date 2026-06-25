b6b4230e9b22ceb31e4b9652aa57697efcf7b068
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2026-06-25 13:37:37 +0300

    Time: 0 ms (100%), Space: 42.7 MB (63.92%) - LeetHub

---
+++ 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean haveConflict(String[] event1, String[] event2) {
+        int start1 = Integer.parseInt(event1[0].substring(0, 2)) * 60 + Integer.parseInt(event1[0].substring(3));
+        int end1 = Integer.parseInt(event1[1].substring(0, 2)) * 60 + Integer.parseInt(event1[1].substring(3));
+        int start2 = Integer.parseInt(event2[0].substring(0, 2)) * 60 + Integer.parseInt(event2[0].substring(3));
+        int end2 = Integer.parseInt(event2[1].substring(0, 2)) * 60 + Integer.parseInt(event2[1].substring(3));
+        return (start1 >= start2 || end1 >= start2) && (start2 >= start1 || end2 >= start1);
+    }
+}
