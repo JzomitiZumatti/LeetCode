2fabb73053e2b95db82309091eb7c98b65af4251
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2026-06-30 15:40:05 +0300

    Time: 6 ms (11.54%), Space: 47.2 MB (13.08%) - LeetHub

---
+++ 3238-find-the-number-of-winning-players/3238-find-the-number-of-winning-players.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int winningPlayerCount(int n, int[][] pick) {
+        Map<Integer, Map<Integer, Integer>> map = new HashMap<>();
+        for (int[] arr : pick) {
+            int player = arr[0];
+            int ball = arr[1];
+            map.putIfAbsent(player, new HashMap<>());
+            map.get(player).put(ball, map.get(player).getOrDefault(ball, 0) + 1);
+        }
+        int ans = 0;
+        for (Map.Entry<Integer, Map<Integer, Integer>> entry : map.entrySet()) {
+            int playerN = entry.getKey();
+            if (playerN == 0) ans++;
+            else {
+                int max = 0;
+                for (Integer val : entry.getValue().values()) {
+                    max = Math.max(max, val);
+                }
+                if (playerN <= max - 1) ans++;
+            }
+        }
+        return ans;
+    }
+}
