e0ec05ea5b2902e51106fbc9cfdc061275f3fa03
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2026-07-17 21:22:03 +0300

    Time: 0 ms (100%), Space: 42.3 MB (12.25%) - LeetHub

---
+++ 1175-prime-arrangements/1175-prime-arrangements.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int numPrimeArrangements(int n) {
+        int MOD = 1_000_000_007;
+        int primesN = 0;
+        for (int i = 1; i <= n; i++) {
+            if (isPrime(i)) primesN++;
+        }
+        return (int) ((factorial(primesN) % MOD) * (factorial(n - primesN)) % MOD) % MOD;
+    }
+
+    private static boolean isPrime(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+
+    private static long factorial(int n) {
+        int MOD = 1_000_000_007;
+        long ans = 1L;
+        for (int i = 1; i <= n; i++) {
+            ans = (ans * i) % MOD;
+        }
+        return ans;
+    }
+}
