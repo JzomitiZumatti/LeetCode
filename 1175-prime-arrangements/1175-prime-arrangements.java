7854026a18eb4002671b791e79ee976b50c34bcc
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2026-07-17 21:23:43 +0300

    Time: 0 ms (100%), Space: 42.3 MB (31.71%) - LeetHub

--- 1175-prime-arrangements/1175-prime-arrangements.java
+++ 1175-prime-arrangements/1175-prime-arrangements.java
@@ -9,8 +9,8 @@ class Solution {
     }
 
     private static boolean isPrime(int n) {
-        if (n < 2) return false;
-        for (int i = 2; i < n; i++) {
+        if (n <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
             if (n % i == 0) return false;
         }
         return true;
