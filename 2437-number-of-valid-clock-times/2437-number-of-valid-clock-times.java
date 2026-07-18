f4302b70d3823508575f6face4855a4dcd48f5b8
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2026-07-18 13:27:35 +0300

    Time: 25 ms (12.5%), Space: 46.6 MB (10.83%) - LeetHub

---
+++ 2437-number-of-valid-clock-times/2437-number-of-valid-clock-times.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int countTime(String time) {
+         String[] hours = new String[24];
+        for (int i = 0; i < 24; i++) {
+            hours[i] = String.format("%02d", i);
+        }
+        String[] minutes = new String[60];
+        for (int i = 0; i < 60; i++) {
+            minutes[i] = String.format("%02d", i);
+        }
+        int ans = 0;
+        for (String hour : hours) {
+            for (String minute : minutes) {
+                String temp = hour + ":" + minute;
+                boolean valid = true;
+                for (int k = 0; k < time.length(); k++) {
+                    if (time.charAt(k) != '?' && time.charAt(k) != temp.charAt(k)) {
+                        valid = false;
+                        break;
+                    }
+                }
+                if (valid) ans++;
+            }
+        }
+        return ans;
+    }
+}
