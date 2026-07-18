ff95e4c0cf35e7d6474920aa2960cad146dc98b9
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2026-07-18 11:31:50 +0300

    Time: 0 ms (100%), Space: 43.4 MB (83.03%) - LeetHub

--- 1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.java
+++ 1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.java
@@ -1,15 +1,15 @@
 class Solution {
     public boolean isCovered(int[][] ranges, int left, int right) {
-        Set<Integer> needed = new HashSet<>();
         for (int i = left; i <= right; i++) {
-            needed.add(i);
-        }
-        for (int[] range : ranges) {
-            for (int j = range[0]; j <= range[1]; j++) {
-                needed.remove(j);
-                if (needed.isEmpty()) return true;
+            boolean isPresent = false;
+            for (int[] range : ranges) {
+                if (i >= range[0] && i <= range[1]) {
+                    isPresent = true;
+                    break;
+                }
             }
+            if (!isPresent) return false;
         }
-        return false;
+        return true;
     }
 }
