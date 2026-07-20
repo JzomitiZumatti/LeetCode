7b793b0fe002d84d799ce1921cf9c151fac61f4b
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2026-07-20 15:31:41 +0300

    Time: 15 ms (24.48%), Space: 48.5 MB (30.82%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.java
+++ 0539-minimum-time-difference/0539-minimum-time-difference.java
@@ -9,13 +9,13 @@ class Solution {
         int ans = Integer.MAX_VALUE;
         if (minutes.size() > 1) {
             for (int i = 1; i < minutes.size(); i++) {
-                int a = minutes.get(i) == 0 ? 1440 : minutes.get(i);
-                int b = minutes.get(i - 1) == 0 ? 1440 : minutes.get(i - 1);
+                int a = minutes.get(i);
+                int b = minutes.get(i - 1);
                 ans = Math.min(ans, Math.abs(a - b));
             }
-            int a = minutes.getLast() == 0 ? 1440 : (1440 - minutes.getLast());
-            int b = minutes.getFirst() == 0 ? 0 : minutes.getFirst();
-            ans = Math.min(ans, Math.abs(a + b));
+            int a = minutes.getLast();
+            int b = minutes.getFirst();
+            ans = Math.min(ans, Math.abs((1440 - a) + b));
             return ans;
         } else return minutes.getFirst();
     }
