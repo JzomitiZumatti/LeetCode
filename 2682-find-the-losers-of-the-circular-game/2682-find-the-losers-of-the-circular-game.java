e0049fab43d84438f082e10ce6e8cac1e0df1262
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2026-07-23 14:46:55 +0300

    Time: 8 ms (5.84%), Space: 46.7 MB (8.76%) - LeetHub

---
+++ 2682-find-the-losers-of-the-circular-game/2682-find-the-losers-of-the-circular-game.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] circularGameLosers(int n, int k) {
+        int[] pa = new int[n];
+        int i = 0;
+        int j = 1;
+        while (true) {
+            pa[i]++;
+            if (pa[i] > 1) break;
+            i = i + (k * j) < n ? i + (k * j) : (i + (k * j)) % n;
+            j++;
+        }
+        List<Integer> ans = new ArrayList<>();
+        for (int l = 0; l < pa.length; l++) {
+            if (pa[l] == 0) ans.add(l + 1);
+        }
+        return ans.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
+    }
+}
