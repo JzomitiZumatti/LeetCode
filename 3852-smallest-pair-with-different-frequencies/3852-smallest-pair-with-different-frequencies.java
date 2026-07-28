1f5c20971799357c600ff2ee1b7fa847f7fda872
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2026-07-28 15:12:42 +0300

    Time: 7 ms (34.87%), Space: 47.1 MB (28.95%) - LeetHub

--- 3852-smallest-pair-with-different-frequencies/3852-smallest-pair-with-different-frequencies.java
+++ 3852-smallest-pair-with-different-frequencies/3852-smallest-pair-with-different-frequencies.java
@@ -1,17 +1,15 @@
 class Solution {
     public int[] minDistinctFreqPair(int[] nums) {
-        Arrays.sort(nums);
         int[] ans = {-1, -1};
         if (nums.length == 1) return ans;
+        Arrays.sort(nums);
         Map<Integer, Integer> digitFreq = new HashMap<>();
         for (int num : nums) {
             digitFreq.put(num, digitFreq.getOrDefault(num, 0) + 1);
         }
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[j] > nums[i] && !Objects.equals(digitFreq.get(nums[i]), digitFreq.get(nums[j])))
-                    return new int[]{nums[i], nums[j]};
-            }
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[0] && !Objects.equals(digitFreq.get(nums[i]), digitFreq.get(nums[0])))
+                return new int[]{nums[0], nums[i]};
         }
         return ans;
     }
