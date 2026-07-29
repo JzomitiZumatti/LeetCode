692f5420117e9819199b2844813393845f80764e
_Ruzvelt_ <103491211+JzomitiZumatti@users.noreply.github.com>
2026-07-29 13:48:00 +0300

    Time: 1 ms (24.06%), Space: 43.4 MB (61.46%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            int current = nums[i];
+            int j = i - 1;
+            while (j >= 0 && nums[j] > current) {
+                nums[j + 1] = nums[j];
+                j--;
+            }
+            nums[j + 1] = current;
+        }
+    }
+}
